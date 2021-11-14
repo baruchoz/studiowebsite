749ed92642b488bc363f07fdf4b79634b1d30cf5
Baruch Rotbart <baruchrotbart@Baruchs-MacBook-Pro.local>
2021-11-13 22:10:56 -0600

    added content to ourteam page

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,25 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    title = "BE Studio's Homepage"
+    title = "Home - BE Studios"
     return render_template("index.html", title=title)
+
+@app.route('/about')
+def about():
+    title = "About - BE Studios"
+    return render_template("about.html", title=title)
+
+@app.route('/ourteam')
+def ourteam():
+    title = "Our Team - BE Studios"
+    return render_template("ourteam.html", title=title)
+
+@app.route('/services')
+def services():
+    title = "Services - BE Studios"
+    return render_template("services.html", title=title)
+
+@app.route('/booking')
+def booking():
+    title = "Booking - BE Studios"
+    return render_template("booking.html", title=title)
