2d587660c893634df7b33e8dd64d6c4fa49f7681
Baruch Rotbart <baruchrotbart@Baruchs-MacBook-Pro.local>
2021-11-17 15:54:39 -0600

    added user login and registration functionality

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField, PasswordField, SubmitField, IntegerField, 
-                    BooleanField, DateTimeField, RadioField, TextAreaField)
+from wtforms import (StringField, PasswordField, SubmitField, IntegerField, BooleanField)
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 from flask_wtf.recaptcha import validators
 from wtforms.widgets.core import TextArea
@@ -14,23 +13,15 @@ class RegisterForm(FlaskForm):
     confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
+
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=15)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=80)])
-    remember = BooleanField('Remember Me')
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Submit')
 
 
-
-class AccountInfoForm(FlaskForm):
-    firstname = StringField('First Name', validators=[DataRequired()])
-    lastname = StringField('Last Name', validators=[DataRequired()])
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    phone = IntegerField('Phone Number', validators=[DataRequired()])
-    address = StringField('Address', validators=[DataRequired()])
-    city = StringField('City', validators=[DataRequired()])
-    state = StringField('State', validators=[DataRequired()])
-    zipcode= StringField('Zipcode', validators=[DataRequired()])
-
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = StringField('Content', validators=[DataRequired(), Email()])
+    submit = SubmitField()
--- app/routes.py
+++ app/routes.py
@@ -1,38 +1,104 @@
-from app import app
+
+from app import app, db
 from flask import render_template, redirect, url_for, flash, session, request
-from app.forms import LoginForm, RegisterForm, AccountInfoForm
+from flask_login import  login_user, logout_user, current_user, login_required
+from app.forms import LoginForm, RegisterForm, PostForm
+from app.models import User, Post
 
 
 @app.route('/')
 def index():
-    title = "Home - BE Studios"
-    return render_template("index.html", title=title)
-
-@app.route('/login')
-def login():
-    title = "Sign In - BE Studios"
-    login_form = LoginForm()
-
-    return render_template("login.html", title=title, form=login_form)
+    return render_template("index.html", title="Home - BE Studios")
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
-    title = "Register - BE Studios"
     register_form = RegisterForm()
-    if register_form.validate_on_submit():
-        print("This form has been submitted correctly")
-        username = register_form.username.data
+    if register_form.validate_on_submit():   
         email = register_form.email.data
+        username = register_form.username.data
         password = register_form.password.data
-        print(username, email, password)
-    return render_template("register.html", title=title, form=register_form)
+        
+        existing_user = User.query.filter_by(username=username).all()
+        if existing_user:
+            # Flash a warning message 
+            flash(f'The username {username} is already registered. Please try again.', 'danger')
+            # Redirect back to the register page
+            return redirect(url_for('register'))
+
+        new_user = User(username, email, password)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash(f'Thank you {username}, you have successfully registered!', 'success')
+        
+        return redirect(url_for('login'))
+
+    return render_template("register.html", title="Register - BE Studios", form=register_form)
+
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+
+        username = login_form.username.data
+        password = login_form.password.data
+
+        user = User.query.filter_by(username=username).first()
+
+        if user is None or not user.check_password(password):
+            flash('Your username or password is incorrect', 'danger')
+            return redirect(url_for('login'))
+
+        login_user(user)
+
+        flash(f'Welcome {user.username}. You have successfully logged in.', 'success')
+        
+        return redirect(url_for('index'))
+
+    return render_template("login.html", title="Sign In - BE Studios", form=login_form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/account') 
+@login_required 
+def my_account():     
+    return render_template('account.html', title="Account - BE Studios")
+
+
+@app.route('/createpost', methods=['GET', 'POST'])
+# @login_required
+def createpost():
+    form = PostForm()
+    if form.validate_on_submit():
+        # print('Hello')
+        title = form.title.data
+        content = form.content.data
+        new_post = Post(title, content, user_id=1)
+        db.session.add(new_post)
+        db.session.commit()
+
+        # flash(f'The post {title} has been created.', 'primary')
+        # return redirect(url_for('index'))
+        
+    return render_template('createpost.html', form=form)
+
 
 
 @app.route('/services')
 def services():
     title = "Services - BE Studios"
     return render_template("services.html", title=title)
+
+
 
 @app.route('/booking')
 def booking():
